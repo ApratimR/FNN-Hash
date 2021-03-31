1a7fd91724ba16d8d7c4c2be2a47552c3d0fea12
Apratim <apratimr55@gmail.com>
2021-03-31 14:37:46 +0530

    package update

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+def readme():
+    with open('readme.md') as f:
+        return f.read()
+    
+setuptools.setup(
+    name="FNNH", # Replace with your own username
+    version="0.0.1",
+    author="Apratim Ray",
+    author_email="apratimr55@gmail.com",
+    description="A flexible CNN based Hash Function",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/apratimr/fnn-hash",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "FNNH"},
+    packages=setuptools.find_packages(where="FNNH"),
+    install_requires = ["numpy"],
+    python_requires=">=3.6",
+)
